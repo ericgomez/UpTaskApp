da38a2dea7995537efddf45ebb6d2be7de56de37
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-01-22 12:01:55 -0600

    Enablig upload static files in Express

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const routes = require('./routes')
 const path = require('path')
+const { use } = require('express/lib/application')
 
 // Create a new express application instance
 const app = express()
+
+// Where to upload static files
+app.use(express.static('public'))
 
 // Activate template engine Pug
 app.set('view engine', 'pug')
