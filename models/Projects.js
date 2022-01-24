bd58515f0cea20e893279cdb763d71c94b895181
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-01-24 17:14:59 -0600

    Creating model of Projects

---
+++ models/Projects.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize')
+
+const db = require('../config/db')
+
+const Projects = db.define('projects', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  url: Sequelize.STRING
+})
+
+module.exports = Projects
