4112bb89d12e01a67e840743257818723c6405e7
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-01-25 11:11:21 -0600

    Using Sequelize Hooks to automatically generate URL

--- models/Projects.js
+++ models/Projects.js
@@ -1,18 +1,30 @@
 const Sequelize = require('sequelize')
+const slug = require('slug')
 
 const db = require('../config/db')
 
-const Projects = db.define('projects', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const Projects = db.define(
+  'projects',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    url: Sequelize.STRING
   },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  url: Sequelize.STRING
-})
+  {
+    hooks: {
+      beforeCreate: project => {
+        const url = slug(project.name).toLowerCase()
+        project.url = url
+      }
+    }
+  }
+)
 
 module.exports = Projects
