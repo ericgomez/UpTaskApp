e4584e4ed53f0a97fa65dff6d3cebbdbb4bc9c75
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-01-31 11:12:28 -0600

    Sending a requestwith Axios to delete a project

--- public/js/modules/projects.js
+++ public/js/modules/projects.js
@@ -3,23 +3,41 @@ import axios from 'axios'
 
 const btnDelete = document.querySelector('#delete-project')
 
-btnDelete.addEventListener('click', e => {
-  Swal.fire({
-    title: 'Are you sure?',
-    text: "You won't be able to revert this!",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete project!'
-  }).then(result => {
-    if (result.isConfirmed) {
-      Swal.fire('Deleted!', 'Your project has been deleted.', 'success')
+if (btnDelete) {
+  btnDelete.addEventListener('click', e => {
+    // data-project-url is a custom attribute
+    // data-project-url = dataset.projectUrl
+    const urlProject = e.target.dataset.projectUrl
 
-      // redirect at home
-      setTimeout(() => {
-        window.location.href = '/'
-      }, 3000)
-    }
+    // console.log(urlProject)
+
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete project!'
+    }).then(result => {
+      if (result.isConfirmed) {
+        // Send the request to the server with axios
+        const url = `${location.origin}/projects/${urlProject}`
+
+        axios.delete(url, { params: urlProject }).then(response => {
+          console.log(response)
+        })
+
+        return
+        Swal.fire('Deleted!', 'Your project has been deleted.', 'success')
+
+        // redirect at home
+        setTimeout(() => {
+          window.location.href = '/'
+        }, 3000)
+      }
+    })
   })
-})
+}
+
+export default btnDelete
