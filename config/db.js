5a70496aac0194358b84886a564ca4970d837965
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-02-03 08:44:00 -0600

    Creating Enviroment variables for the database

--- config/db.js
+++ config/db.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require('sequelize')
+require('dotenv').config()
 
 // Passing parameters separately
-const db = new Sequelize('tasks_app', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mariadb'
-})
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mariadb',
+    port: process.env.DB_PORT
+  }
+)
 
 module.exports = db
